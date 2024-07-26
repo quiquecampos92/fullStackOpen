416b27727008e20fb1b0e3e01f96f23676aeab78
“Quique” <quiquecampos92@outlook.com>
2024-07-26 15:37:35 +0200

    part3 e.3.7 paso 7 PORT modified

--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const basedUrl = 'http://localhost:3001/persons'
+const basedUrl = 'http://localhost:3001/api/persons'
 
 const getAllContacts = () => {
     return axios.get(basedUrl)
