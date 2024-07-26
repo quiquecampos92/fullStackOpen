e67fb26ee48da140dd0133cb27d541f6e49471d4
“Quique” <quiquecampos92@outlook.com>
2024-07-26 17:39:59 +0200

    part3 e.3.11 paso 11

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
-app.use(express.static('dist'))
-app.use(express.json());
-app.use(morgan('tiny'))
+
 
 
 let contacts = [
@@ -29,6 +28,12 @@ let contacts = [
         "number": "39-23-6423122"
     }
 ]
+app.use(express.static('dist'))
+app.use(cors())
+
+app.use(express.json());
+app.use(morgan('tiny'))
+
 
 app.get('/', (request, response) => {
     response.send('<h1>PhoneBook</h1>')
